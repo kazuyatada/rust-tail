a58d3eda426c179e239f4eb2c582fbc5cc1a0cfb
kazuyatada <kazuyatada87@gmail.com>
2021-08-28 12:44:09 +0900

    update: ファイルの中身を１行ずつ読み取る

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::env;
-use std::fs;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
     let filename = &args[1];
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
 
-    println!("File contents:\n{}", contents);
+    println!("File contents:");
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+    Ok(())
 }
