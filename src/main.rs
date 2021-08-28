926c247bdb7c26d4fd1a0b68b0de237483bde522
kazuyatada <kazuyatada87@gmail.com>
2021-08-28 10:49:28 +0900

    update: 引数を１つ取り、そのファイルの中身を表示する

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let filename = &args[1];
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("File contents:\n{}", contents);
 }
